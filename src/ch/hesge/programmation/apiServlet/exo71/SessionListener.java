b339dc4ce3cea3400298bdce82eeb83bf9e0ce62
mikee-lp <elv-john.qnt@eduge.ch>
2019-03-16 16:08:39 +0100

    first commit

---
+++ src/ch/hesge/programmation/apiServlet/exo71/SessionListener.java
@@ -0,0 +1,20 @@
+package ch.hesge.programmation.apiServlet.exo71;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+@WebListener
+public class SessionListener implements HttpSessionListener {
+    @Override
+    public void sessionCreated(HttpSessionEvent httpSessionEvent) {
+        /*System.out.println("new session created");
+        HttpSession session = httpSessionEvent.getSession();
+        System.out.println("session id : " + session.getId());*/
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+
+    }
+}
